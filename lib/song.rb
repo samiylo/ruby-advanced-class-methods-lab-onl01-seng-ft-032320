9a33c9d0e72d1063b465207cc96b4952fbe46e47
Samiylo Kryshu <skryshu@gmail.com>
2020-04-02 19:36:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,5 +49,13 @@ class Song
     song.artist_name = (name.split(" - ")[0])
     song
   end
+  
+  def self.create_from_filename(name)
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    @@all << song
+    song
+  end
 
 end

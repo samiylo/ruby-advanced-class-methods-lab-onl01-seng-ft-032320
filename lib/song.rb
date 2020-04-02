2d65609a653256c5da04c7455862f246d71000e2
Samiylo Kryshu <skryshu@gmail.com>
2020-04-02 19:36:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,12 +43,12 @@ class Song
     @@all.sort_by {|n| n.name}
   end
   
-  def self.new_from_filename(name)
-    song = self.new 
-    song.name = (name.split(" - ")[1].chomp(".mp3"))
-    song.artist_name = (name.split(" - ")[0])
-    song
-  end
+  # def self.new_from_filename(name)
+  #   song = self.new 
+  #   song.name = (name.split(" - ")[1].chomp(".mp3"))
+  #   song.artist_name = (name.split(" - ")[0])
+  #   song
+  # end
   
   def self.create_from_filename(name)
     song = self.new
